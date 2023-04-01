5cf9868cd542becf198defb8ca66f644d123ef30
SonjaTuro <sonja.turo@rout.net>
2023-04-01 22:11:44 +1100

    Refactor QuackGPT

--- src/SpeciesViewProvider.ts
+++ src/SpeciesViewProvider.ts
@@ -1,10 +1,11 @@
 import {
-  CancellationToken, Disposable, OutputChannel, Uri, Webview,
+  CancellationToken, Disposable, Uri, Webview,
   WebviewView, WebviewViewProvider,
   WebviewViewResolveContext, window
 } from "vscode";
 import { DuckMessage } from "./commands/DuckModeQuack";
 import Audio from "./ducklibs/Audio";
+import QuackGPT from "./ducklibs/QuackGPT";
 
 export default class SpeciesViewProvider implements WebviewViewProvider {
 
@@ -12,7 +13,7 @@ export default class SpeciesViewProvider implements WebviewViewProvider {
 
   private _view?: WebviewView;
   private _audio: Audio;
-  private _quackGPTchannel?: OutputChannel;
+  private _quackGPT: QuackGPT = new QuackGPT();
 
   constructor(
     private readonly _extensionUri: Uri
@@ -38,6 +39,14 @@ export default class SpeciesViewProvider implements WebviewViewProvider {
     }
   }
 
+  /**
+   * This is called automatically by VsCode every time the view is shown as a new object,
+   * so it needs registering each time
+   * 
+   * @param webviewView
+   * @param context 
+   * @param token 
+   */
   public resolveWebviewView(
     webviewView: WebviewView,
     context: WebviewViewResolveContext,
@@ -45,6 +54,7 @@ export default class SpeciesViewProvider implements WebviewViewProvider {
   ): void | Thenable<void> {
     this._view = webviewView;
     this._audio.webviewView = webviewView;
+    this._quackGPT.webviewView = webviewView;
 
     webviewView.webview.options = {
         enableScripts: true,
@@ -55,125 +65,77 @@ export default class SpeciesViewProvider implements WebviewViewProvider {
     };
 
     webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
-
-    webviewView.webview.onDidReceiveMessage(data => {
-      window.showInputBox({
-        prompt: 'Ask QuackGPT showcases the latest breakthroughs in duck centric AI. Ask me anything!'
-      }).then((value) => {
-        if (value === undefined) {
-          return;
-        }
-        this.getQuackGPTChannel().appendLine(this.getQuackGPTResponse(value || 'Quack.'));
-        this.getQuackGPTChannel().show();
-        if (this._view) {
-          this._view.show?.(true);
-          this._view.webview.postMessage({ type: 'audioQuack'});
-        }
-      })
-    });
-  }
-
-  protected getQuackGPTChannel() : OutputChannel {
-    if(!this._quackGPTchannel) {
-      this._quackGPTchannel = window.createOutputChannel('QuackGPT');
-    }
-
-    return this._quackGPTchannel;
-  }
-
-  protected getQuackGPTResponse(value: string) : string {
-    const sentences = new Array(Math.ceil(Math.random() * 3));
-    for (let i = 0; i < sentences.length; i++) {
-      sentences[i] = this.getSentence();
-    }
-
-    return sentences.join(this.getSentenceEnd() + ' ') + this.getSentenceEnd();
-  }
-
-  protected getSentence() {
-    const quacks = new Array(Math.ceil(Math.random() * 20) + 2);
-    for (let i = 0; i < quacks.length; i++) {
-      quacks[i] =  i === 0 ? 'Quack': 'quack';
-    }
-
-    return quacks.join(' ');
-  }
-
-  protected getSentenceEnd() : string
-  {
-    return Math.ceil(Math.random() * 100) <= 70 ? '.' :
-      (Math.ceil(Math.random() * 100) <= 60 ? '!' : '?');
   }
 
   private _getHtmlForWebview(webview: Webview) {
 
-  // Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
-  const scriptUri = webview.asWebviewUri(Uri.joinPath(this._extensionUri, 'resources', 'species-view.js'));
+    // Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
+    const scriptUri = webview.asWebviewUri(Uri.joinPath(this._extensionUri, 'resources', 'species-view.js'));
 
-  // Do the same for the stylesheet.
-  const styleResetUri = webview.asWebviewUri(Uri.joinPath(this._extensionUri, 'resources', 'reset.css'));
-  const styleVSCodeUri = webview.asWebviewUri(Uri.joinPath(this._extensionUri, 'resources', 'vscode.css'));
-  const styleMainUri = webview.asWebviewUri(Uri.joinPath(this._extensionUri, 'resources', 'species-view.css'));
-  const codiconsUri = webview.asWebviewUri(Uri.joinPath(this._extensionUri, 'node_modules', '@vscode/codicons', 'dist', 'codicon.css'));
+    // Do the same for the stylesheet.
+    const styleResetUri = webview.asWebviewUri(Uri.joinPath(this._extensionUri, 'resources', 'reset.css'));
+    const styleVSCodeUri = webview.asWebviewUri(Uri.joinPath(this._extensionUri, 'resources', 'vscode.css'));
+    const styleMainUri = webview.asWebviewUri(Uri.joinPath(this._extensionUri, 'resources', 'species-view.css'));
+    const codiconsUri = webview.asWebviewUri(Uri.joinPath(this._extensionUri, 'node_modules', '@vscode/codicons', 'dist', 'codicon.css'));
 
-  const quackMp3Uri = webview.asWebviewUri(Uri.joinPath(this._extensionUri, 'resources', 'quack.mp3'));
-  const duckImageUri = webview.asWebviewUri(Uri.joinPath(this._extensionUri, 'resources'));
+    const quackMp3Uri = webview.asWebviewUri(Uri.joinPath(this._extensionUri, 'resources', 'quack.mp3'));
+    const duckImageUri = webview.asWebviewUri(Uri.joinPath(this._extensionUri, 'resources'));
 
-  // Use a nonce to only allow a specific script to be run.
-  const nonce = this.getNonce();
+    // Use a nonce to only allow a specific script to be run.
+    const nonce = this.getNonce();
 
-  return `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}'; media-src ${webview.cspSource}; img-src ${webview.cspSource}; font-src ${webview.cspSource}">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link href="${styleResetUri}" rel="stylesheet">
-      <link href="${styleVSCodeUri}" rel="stylesheet">
-      <link href="${styleMainUri}" rel="stylesheet">
-      <link href="${codiconsUri}" rel="stylesheet">
-      <title>Duck Mode</title>
-    </head>
-    <body>
-      <button appearance="secondary" id="sound-button" aria-label="Unmute">
-          Ask <span class="codicon codicon-unmute"></span>
-      </button>
+    return `<!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}'; media-src ${webview.cspSource}; img-src ${webview.cspSource}; font-src ${webview.cspSource}">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="${styleResetUri}" rel="stylesheet">
+        <link href="${styleVSCodeUri}" rel="stylesheet">
+        <link href="${styleMainUri}" rel="stylesheet">
+        <link href="${codiconsUri}" rel="stylesheet">
+        <title>Duck Mode</title>
+      </head>
+      <body>
+        <button appearance="secondary" id="sound-button" aria-label="Unmute">
+            Ask <span class="codicon codicon-unmute"></span>
+        </button>
 
-      <button appearance="primary" id="quackgpt-button" aria-label="Ask QuackGPT a question." >
-          Ask QuackGPT
-      </button>
+        <button appearance="primary" id="quackgpt-button" aria-label="Ask QuackGPT a question." >
+            Ask QuackGPT
+        </button>
 
-      <audio src="${quackMp3Uri}" id="audio-quack"></audio>
-      <h4>Alabio</h4>
-      <h6><i>Anas platyrhynchos Borneo</i></h6>
-      <img src="${duckImageUri}/alabio.jpg">
-      <h4>Bufflehead</h4>
-      <h6><i>Bucephala albeola</i></h6>
-      <img src="${duckImageUri}/bufflehead.jpg">
-      <h4>Fulvous Whistling</h4>
-      <h6><i>Dendrocygna bicolor</i></h6>
-      <img src="${duckImageUri}/fulvous-whistling.jpg">
-      <h4>Mallard</h4>
-      <h6><i>Anas platyrhynchos</i></h6>
-      <img src="${duckImageUri}/mallard.jpg">
-      <h4>Marbled</h4>
-      <h6><i>Marmaronetta angustirostris</i></h6>
-      <img src="${duckImageUri}/marbled.jpg">
-      <h4>Merganser</h4>
-      <h6><i>Mergus serrator</i></h6>
-      <img src="${duckImageUri}/merganser.jpg">
-      <h4>Muscovy</h4>
-      <h6><i>Cairina moschata</i></h6>
-      <img src="${duckImageUri}/muscovy.jpg">
-      <h4>Ring Necked</h4>
-      <h6><i>Aythya collaris</i></h6>
-      <img src="${duckImageUri}/ring-necked.jpg">
-      <h4>White backed</h4>
-      <h6><i>Thalassornis Leuconotus</i></h6>
-      <img src="${duckImageUri}/white-backed.jpg">
-      <script nonce="${nonce}" src="${scriptUri}"></script>
-    </body>
-    </html>`;
+        <audio src="${quackMp3Uri}" id="audio-quack"></audio>
+        <h4>Alabio</h4>
+        <h6><i>Anas platyrhynchos Borneo</i></h6>
+        <img src="${duckImageUri}/alabio.jpg">
+        <h4>Bufflehead</h4>
+        <h6><i>Bucephala albeola</i></h6>
+        <img src="${duckImageUri}/bufflehead.jpg">
+        <h4>Fulvous Whistling</h4>
+        <h6><i>Dendrocygna bicolor</i></h6>
+        <img src="${duckImageUri}/fulvous-whistling.jpg">
+        <h4>Mallard</h4>
+        <h6><i>Anas platyrhynchos</i></h6>
+        <img src="${duckImageUri}/mallard.jpg">
+        <h4>Marbled</h4>
+        <h6><i>Marmaronetta angustirostris</i></h6>
+        <img src="${duckImageUri}/marbled.jpg">
+        <h4>Merganser</h4>
+        <h6><i>Mergus serrator</i></h6>
+        <img src="${duckImageUri}/merganser.jpg">
+        <h4>Muscovy</h4>
+        <h6><i>Cairina moschata</i></h6>
+        <img src="${duckImageUri}/muscovy.jpg">
+        <h4>Ring Necked</h4>
+        <h6><i>Aythya collaris</i></h6>
+        <img src="${duckImageUri}/ring-necked.jpg">
+        <h4>White backed</h4>
+        <h6><i>Thalassornis Leuconotus</i></h6>
+        <img src="${duckImageUri}/white-backed.jpg">
+        <script nonce="${nonce}" src="${scriptUri}"></script>
+      </body>
+      </html>`;
   }
 
   getNonce() {
---
+++ src/ducklibs/QuackGPT.ts
@@ -0,0 +1,76 @@
+import { Disposable, OutputChannel, WebviewView, window } from "vscode";
+import { DuckMessage } from "../commands/DuckModeQuack";
+
+export default class QuackGPT{
+
+  private _webviewView?: WebviewView;
+  private _quackGPTchannel?: OutputChannel;
+
+  private audio: DuckMessage = {
+    type: 'audio',
+    domElementId: 'audio-quack'
+  }
+
+  constructor() {
+    this._quackGPTchannel = window.createOutputChannel('QuackGPT');
+  }
+
+  set webviewView(webviewView: WebviewView) {
+    this._webviewView = webviewView;
+
+    webviewView.webview.onDidReceiveMessage(data => {
+        window.showInputBox({
+            prompt: 'Ask QuackGPT showcases the latest breakthroughs in duck centric AI. Ask me anything!'
+        }).then((value) => {
+            if (value === undefined || this._quackGPTchannel === undefined) {
+              return;
+            }
+            this._quackGPTchannel.appendLine(this.getQuackGPTResponse(value || 'Quack.'));
+            this._quackGPTchannel.show();
+            this.quack(this.audio);
+        })
+    });
+  }
+
+  public quack(quack: DuckMessage): void {
+    if (this._webviewView) {
+      this.showView();
+      this._webviewView.webview.postMessage(quack);
+    }
+  }
+
+  /**
+   * Ensures that the webview is visible onscreen before attempting to
+   * play audio, otherwise it won't work.
+   */
+  protected showView(): void {
+    if (this._webviewView && this._webviewView.show) {
+      this._webviewView.show(true);
+    }
+  }
+
+  protected getQuackGPTResponse(value: string) : string {
+    const sentences = new Array(Math.ceil(Math.random() * 3));
+    for (let i = 0; i < sentences.length; i++) {
+      sentences[i] = this.getSentence();
+    }
+
+    return sentences.join(this.getSentenceEnd() + ' ') + this.getSentenceEnd();
+  }
+
+  protected getSentence() {
+    const quacks = new Array(Math.ceil(Math.random() * 20) + 2);
+    for (let i = 0; i < quacks.length; i++) {
+      quacks[i] =  i === 0 ? 'Quack': 'quack';
+    }
+
+    return quacks.join(' ');
+  }
+
+  protected getSentenceEnd() : string
+  {
+    return Math.ceil(Math.random() * 100) <= 70 ? '.' :
+      (Math.ceil(Math.random() * 100) <= 60 ? '!' : '?');
+  }
+
+}
